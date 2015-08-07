049af49f009d4650e08eaec9730f5bf3377af11b
Andrew.Wang <wangguohui@lianjia.com>
2015-08-07 11:04:25 +0800

    1

---
+++ gitconfig.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#make sure git exists
+git > /dev/null
+if [ "$?" -eq "127" ];then #git not exists
+    echo 'git not exist'
+    exit 0
+fi
+
+git config --global diff.tool vimdiff
+git config --global alias.st status
+git config --global alias.ci commit
+git config --global alias.co checkout
+
+
